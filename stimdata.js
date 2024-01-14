45559a66ab21fb86e8cf0ce2d986589ea6e0795c
ClorisT2023 <155012201+ClorisT2023@users.noreply.github.com>
2024-01-15 03:38:35 +0800

    Update stimdata.js

--- stimdata.js
+++ stimdata.js
@@ -1,6 +1,6 @@
 lex_dict = {
     'en': [
-        { 'word': '/ch_items/1_True_55.png', 'wstatus': 0, 'dummy': 1 },
+        { 'word': '隧', 'wstatus': 0, 'dummy': 1 },
         { 'word': 'denial', 'wstatus': 1, 'dummy': 1 },
         { 'word': 'generic', 'wstatus': 1, 'dummy': 1 },
         { 'word': 'mensible', 'wstatus': 0, 'dummy': 0 },
